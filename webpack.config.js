ead0c8c0f6b87782f52454e0ac78eb1a36a89530
unknown <kostya.mkhalkov@bk.ru>
2024-04-12 14:37:15 +0300

    feat: add from slider HTML and CSS

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const { watch } = require('fs');
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/script.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    watch: true
+}
